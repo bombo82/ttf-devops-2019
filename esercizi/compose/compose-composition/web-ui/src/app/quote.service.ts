fd953eee360983a88a2de692ce9630eff340cb32
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2021-03-12 12:29:19 +0100

    add internal environment configurations
    Signed-off-by: Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>

--- esercizi/compose/compose-composition/web-ui/src/app/quote.service.ts
+++ esercizi/compose/compose-composition/web-ui/src/app/quote.service.ts
@@ -2,20 +2,19 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Quote} from './quote';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuoteService {
 
-  url = 'http://localhost:3000/';
-
   constructor(private httpClient: HttpClient) { }
   list(): Observable<Quote[]> {
-    return this.httpClient.get<Quote[]>(this.url);
+    return this.httpClient.get<Quote[]>(environment.apiUrl);
   }
 
   create(model: Quote): Observable<Quote | string> {
-    return this.httpClient.post<Quote | string>(this.url, model);
+    return this.httpClient.post<Quote | string>(environment.apiUrl, model);
   }
 }
